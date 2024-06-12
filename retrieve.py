9aec1c0f4d74bce5d707dce284a5282aa3293b96
Pearl <pnatalia@uwaterloo.ca>
2024-06-11 23:13:04 -0400

    Working version of storing device's video footage

--- retrieve.py
+++ retrieve.py
@@ -1,27 +1,54 @@
 import cv2
+import numpy as np
+import os
 
-# Open the video file for reading
-cap = cv2.VideoCapture('output.avi')
+FILE_OUTPUT = 'output.avi'
 
-# Check if the video file was successfully opened
-if not cap.isOpened():
-    print("Error: Could not open video file")
-    exit()
+# Checks and deletes the output file
+# You cant have a existing file or it will through an error
+if os.path.isfile(FILE_OUTPUT):
+    os.remove(FILE_OUTPUT)
 
-# Read and display each frame of the video
-while True:
+# Playing video from file:
+# cap = cv2.VideoCapture('vtest.avi')
+# Capturing video from webcam:
+cap = cv2.VideoCapture(0)
+
+currentFrame = 0
+
+# Get current width of frame
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+# Get current height of frame
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)   # float
+
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'X264')
+out = cv2.VideoWriter(FILE_OUTPUT,fourcc, 20.0, (int(width),int(height)))
+
+# while(True):
+while(cap.isOpened()):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if ret:
-        # Display the frame
-        cv2.imshow('Video', frame)
-        # Wait for a specific delay (in milliseconds)
-        # Press 'q' to exit the video playback
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+
+    if ret == True:
+        # Handles the mirroring of the current frame
+        frame = cv2.flip(frame,1)
+
+        # Saves for video
+        out.write(frame)
+
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
     else:
-        # End of the video
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture object and close all OpenCV windows
+    # To stop duplicate images
+    currentFrame += 1
+
+# When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
